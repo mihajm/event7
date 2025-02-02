652903d16bf9a90cab9db536c7c0cadab93f90e6
Miha Mulec <miha.mulec@marand.si>
2025-02-02 11:43:26 +0100

    infra: setup material

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,7 +1,9 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import {
   type ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
@@ -9,5 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
   ],
 };
