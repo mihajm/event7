ed8811e0bbfbb313689b97cacba7b02b5a07753f
miham <miham@marand.si>
2025-02-04 09:20:00 +0100

    feat: resource cache

---
+++ libs/common/http/src/lib/resource-cache.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Injectable } from '@angular/core';
+import { Cache } from '@e7/common/cache';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ResourceCache {
+  private readonly internal = new Cache<any>();
+
+  private getCache<T>() {
+    return this.internal as Cache<T>;
+  }
+
+  get<T>(key: string) {
+    return this.getCache<T>().get(key);
+  }
+
+  store<T>(key: string, value: T, staleTime: number, ttl: number) {
+    return this.getCache<T>().storeWithInvalidation(key, value, staleTime, ttl);
+  }
+
+  changes$<T>(key: string) {
+    return this.getCache<T>().changes$(key);
+  }
+}
